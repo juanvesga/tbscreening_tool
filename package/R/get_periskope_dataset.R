148a9507b3408837714eb990e65c84c3a073abb5
Tim Taylor <tim.taylor@hiddenelephants.co.uk>
2024-03-27 14:52:43 +0000

    minor tidying of get_periskope_dataset()

--- package/R/get_periskope_dataset.R
+++ package/R/get_periskope_dataset.R
@@ -2,34 +2,23 @@ get_periskope_dataset<-function(data, prevalence_tab, age.categorical){
 
     cohort_size<-data$cohort_size
     test<-data$test
-
     cohort_type<-data$cohort_mode
     highburden<-data$burden_100k
     contact_type<-data$contact_type
-
-
-    #country <- data$country
     time_horizon<-data$t_hor
-
     prev<-prevalence_tab[,1]/100
 
     # Global
-    lookage <- read.table(header = TRUE,
-                          stringsAsFactors = FALSE,
-                          text="Age_cat Age
-  16-35         21
-  36-45         36
-  46-65         56
-  65+         83")
+    lookage <- data.frame(
+        Age_cat = c("16-35", "36-45", "46-65", "65+"),
+        Age     = c(    21L,     36L,     56L,   83L)
+    )
 
     # Sample from categorical distribution
     samps_age<-distributions3::random(age.categorical, cohort_size)
+    largetable <- data.frame(Age_cat = samps_age)
 
-    largetable <- data.frame(
-        Age_cat = samps_age,
-        stringsAsFactors = FALSE)
-
-    base1 <- (merge(largetable,lookage,  by = 'Age_cat'))
+    base1 <- merge(largetable,lookage,  by = 'Age_cat')
     base1$result<-"Negative"
 
     na1<-which(base1$Age_cat=="16-35")
@@ -51,8 +40,6 @@ get_periskope_dataset<-function(data, prevalence_tab, age.categorical){
 
     npositives<-length(which(base1$result=="Positive"))
 
-
-
     # Results
     base1$pct_qfn <-NA
     base1$pct_tspot <- NA
@@ -61,25 +48,28 @@ get_periskope_dataset<-function(data, prevalence_tab, age.categorical){
     base1$tspot_result <- NA
     base1$tst_result <- NA
 
-    if(test=="QuantiFERON"){
+    if(test=="QuantiFERON") {
         base1$qfn_result <-base1$result
-    }else if(test=="T-SPOT.TB"){
+    } else if(test=="T-SPOT.TB") {
         base1$tspot_result <-base1$result
-
     }else{
         base1$tst_result <-base1$result
-
     }
+
     # Impute result from qualitative result
-    base1 <- base1 %>% dplyr::mutate(pct_testspl1 = dplyr::case_when(!is.na(pct_qfn) ~ as.integer(pct_qfn),
-                                                       !is.na(pct_tspot) ~ as.integer(pct_tspot),
-                                                       qfn_result=="Positive" ~ as.integer(87),
-                                                       qfn_result=="Negative" ~ as.integer(1),
-                                                       tspot_result=="Positive" ~ as.integer(87),
-                                                       tspot_result=="Borderline positive" ~ as.integer(79),
-                                                       tspot_result=="Borderline negative" ~ as.integer(76),
-                                                       tspot_result=="Negative" ~ as.integer(1),
-                                                       !is.na(pct_tst) ~ as.integer(pct_tst)))
+    base1 <- dplyr::mutate(
+        base1,
+        pct_testspl1 = dplyr::case_when(
+            !is.na(pct_qfn) ~ as.integer(pct_qfn),
+            !is.na(pct_tspot) ~ as.integer(pct_tspot),
+            qfn_result=="Positive" ~ 87L,
+            qfn_result=="Negative" ~ 1L,
+            tspot_result=="Positive" ~ 87L,
+            tspot_result=="Borderline positive" ~ 79L,
+            tspot_result=="Borderline negative" ~ 76L,
+            tspot_result=="Negative" ~ 1L,
+            !is.na(pct_tst) ~ as.integer(pct_tst))
+    )
 
     ### Add test result splines (5 knots at fixed positions)
     pct_test_spline5 <- as.data.frame(Hmisc::rcspline.eval(base1$pct_testspl1, knots = c(5, 27.5, 50, 72.5, 95)))
@@ -87,24 +77,18 @@ get_periskope_dataset<-function(data, prevalence_tab, age.categorical){
     base1 <- data.frame(base1,pct_test_spline5)
 
     ## Age splines (5 knots at fixed positions)
-    base1 <- base1 %>% dplyr::rename(agespl1=Age)
+    base1 <- dplyr::rename(base1, agespl1=Age)
     age_spline5 <- as.data.frame(Hmisc::rcspline.eval(base1$agespl1, knots = c(8, 25, 33.07, 45, 64)))
     colnames(age_spline5) <- c("agespl2", "agespl3", "agespl4")
     base1 <- data.frame(base1,age_spline5)
 
-
-
     # Status
-    base1$contact<-if(cohort_type=="contact"){ "Yes" }else{ "No"}
+    base1$contact<-if (cohort_type=="contact") {"Yes"} else {"No"}
     base1$indexcase_proximity<-if(cohort_type=="contact")
-    { contact_type }else{ "Not applicable"}
-    base1$migrant<-if(cohort_type=="new"){ "Yes" }else{ "No"}
-
-
-
+        { contact_type } else { "Not applicable"}
+    base1$migrant <- if (cohort_type=="new") {"Yes"} else {"No"}
 
     # Exposure category
-
     if(cohort_type=="contact"&& contact_type=="household"){
 
         base1$exposure_cat4b <- "Household, smear+"
@@ -112,6 +96,7 @@ get_periskope_dataset<-function(data, prevalence_tab, age.categorical){
     } else if(cohort_type=="contact"&& contact_type=="other"){
 
         base1$exposure_cat4b <-  "Other contacts"
+
     }else{
 
         base1$exposure_cat4b <- "No contact, non-migrant"
@@ -135,15 +120,17 @@ get_periskope_dataset<-function(data, prevalence_tab, age.categorical){
     base1$transplant<-"No"
     base1$ltbi_treatment<-"No"
 
-    base1 <- base1 %>% dplyr::select(agespl1, agespl2, agespl3, agespl4,
-                              pct_testspl1, pct_testspl2, pct_testspl3, pct_testspl4,
-                              exposure_cat4b, months_migrant,
-                              hivpos, transplant,
-                              ltbi_treatment, qfn_result, tspot_result) %>%
-        dplyr::rename(transplant_assumed=transplant)
+    base1 <- dplyr::select(
+        base1,
+        agespl1, agespl2, agespl3, agespl4,
+        pct_testspl1, pct_testspl2, pct_testspl3, pct_testspl4,
+        exposure_cat4b, months_migrant,
+        hivpos, transplant_assumed = transplant,
+        ltbi_treatment, qfn_result, tspot_result
+    )
 
-    res<-list()
-    res$df=base1
-    res$npositives=npositives
-    return(res)
+    list(
+        df = base1,
+        npositives = npositives
+    )
 }
